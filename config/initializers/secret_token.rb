b85398d52c7f98aab2a42f2a41809093d6341f17
Marc Chung <mchung@gmail.com>
2013-02-23 13:24:42 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smartnetwork::Application.config.secret_token = '4a7371edea1ace114a89d9ee289a63328237932557e456c96aa132dadd2f0960088ef521611b2cec28dda3e9e1b7bc385085b81af4fcdb15006ab56d84d7a982'
